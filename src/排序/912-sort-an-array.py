e365e735dba07d4d8d64b8d6bf701e5dd1e5756d
ZJsnowman <zj2299490@gmail.com>
2020-11-10 19:43:22 +0800

    排序

--- src/排序/912-sort-an-array.py
+++ src/排序/912-sort-an-array.py
@@ -19,6 +19,7 @@ class Solution:
                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
         return nums
 
+    # 快排 时间复杂度度 O(nlogn)
     def quick_sort(self, nums):
         if len(nums) < 2:
             return nums
@@ -28,6 +29,56 @@ class Solution:
             less_part = [i for i in nums[pivot_index + 1:] if i < pivot]
             great_part = [i for i in nums[pivot_index + 1:] if i >= pivot]
             return self.quick_sort(less_part) + [pivot] + self.quick_sort(great_part)
+
+    def quick_sort_inplace(self, nums, start, end):  # 左闭右开
+        if start < end:
+            pivot = self.partition(nums, start, end)
+            self.quick_sort_inplace(nums, start, pivot)
+            self.quick_sort_inplace(nums, pivot + 1, end)
+
+        print(nums)
+
+    def partition(self, nums, start, end):
+        pivot_index = start
+        pivot = nums[start]
+        left = start + 1
+        right = end - 1
+
+        while True:
+            while left <= right and nums[left] < pivot:
+                left += 1
+            while left <= right and nums[right] >= pivot:
+                right -= 1
+            if left > right:
+                break
+            else:
+                nums[left], nums[right] = nums[right], nums[left]
+        nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
+        return right
+
+    def merge_sort(self, nums):
+        if len(nums) <= 1:
+            return nums
+
+        mid = len(nums) // 2
+        left = self.merge_sort(nums[:mid])
+        right = self.merge_sort(nums[mid:])
+        # 合并两个有序数组
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        new_sorted_list = []
+        i = j = 0
+        while (i < len(left) and j < len(right)):
+            if left[i] <= right[j]:
+                new_sorted_list.append(left[i])
+                i += 1
+            else:
+                new_sorted_list.append(right[j])
+                j += 1
+        new_sorted_list += left[i:]
+        new_sorted_list += right[j:]
+        return new_sorted_list
 
 
 if __name__ == '__main__':
@@ -40,5 +91,9 @@ if __name__ == '__main__':
     # print(sol.bubble_sort(nums1))
     # print(sol.bubble_sort(nums2))
 
-    print(sol.quick_sort(nums1))
-    print(sol.quick_sort(nums2))
+    # print(sol.quick_sort(nums1))
+    # print(sol.quick_sort(nums2))
+    # sol.quick_sort_inplace(nums1, 0, len(nums1))
+    # sol.quick_sort_inplace(nums2, 0, len(nums2))
+    print(sol.merge_sort(nums1))
+    print(sol.merge_sort(nums2))
